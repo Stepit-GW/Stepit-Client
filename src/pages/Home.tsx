ed8939d3acc1900be2add6d8a22acdd602af548e
choiyeju <yeju1019@gmail.com>
2023-03-23 11:23:35 +0900

    feat: video 다루는법

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,86 +10,63 @@ import {
   TextInput,
   View,
 } from 'react-native';
-import {
-  GRAY,
-  MARGIN_HOR,
-  MARGIN_VER,
-  PINK0,
-  PINK3,
-  WINDOW_HEIGHT,
-  WINDOW_WIDTH,
-} from '@/static/commonValue';
 import {commonStyles} from '@/styels/commonStyles';
-import Charbar1 from '@/components/home/Chatbar1';
-import Charbar2 from '@/components/home/Charbar2';
-import HomeContents from '@/components/home/HomeContents';
-import ModalRoomDetail from '@/components/home/ModalRoomDetail';
+import Video from 'react-native-video';
 
 export default function Home({navigation}: any): JSX.Element {
-  const [visible, setVisible] = useState(false);
+  const [videoStart, setVideoStart] = useState(false);
 
-  const aniHeight = useRef<Animated.Value>(new Animated.Value(0)).current;
-  const aniOpacity = useRef<Animated.Value>(new Animated.Value(0)).current;
-  const aniLeftRight = useRef<Animated.Value>(new Animated.Value(0)).current;
-  const aniMargin = useRef<Animated.Value>(
-    new Animated.Value(WINDOW_HEIGHT / 2.2),
-  ).current;
-  const aniSearchFn = (h: number, m: number, lf: number, o: number) => {
-    Animated.timing(aniHeight, {
-      toValue: h,
-      duration: 1000,
-      useNativeDriver: false,
-    }).start();
-    Animated.timing(aniMargin, {
-      toValue: m,
-      duration: 1000,
-      useNativeDriver: false,
-    }).start();
-    Animated.timing(aniLeftRight, {
-      toValue: lf,
-      duration: 1000,
-      useNativeDriver: false,
-    }).start();
-    Animated.timing(aniOpacity, {
-      toValue: o,
-      duration: 1000,
-      useNativeDriver: false,
-    }).start();
-  };
-
-  const isValidToken = async (refreshToken: string) => {
-    // await axios
-    //   .get('http://54.180.188.181:8080/api/auth/validity', {
-    //     params: {refreshToken: refreshToken},
-    //   })
-    //   .then(({data}) => data)
-    //   .catch(err => console.log(err.response));
-  };
-
-  const [test, setTest] = useState(true);
-  const getToken = async () => {
-    // const refresh = await AsyncStorage.getItem('refreshToken');
-    // if (refresh !== null) {
-    //   const data = JSON.parse(refresh);
-    //   if (!isValidToken(data)) {
-    //     navigation.navigate('Logo');
-    //   }
-    // } else {
-    //   navigation.navigate('Logo');
-    // }
-    if (test) {
-    } else navigation.navigate('Logo');
-  };
-
-  useEffect(() => {
-    getToken();
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <SafeAreaView style={commonStyles.container}>
-      <View style={commonStyles.containerView}></View>
+      <View style={commonStyles.containerView}>
+        {/* <Pressable
+          onPress={() => {
+            console.log('a');
+            setVideoStart(true);
+            // setTimeout(() => {
+            //   setVideoStart(false);
+            // }, 500);
+          }}> */}
+        <Video
+          source={require('@/assets/test.mp4')}
+          style={styles.fullScreen}
+          paused={videoStart} // 재생/중지 여부, 디비에서 시간을 보내주고 setTimeout이용해서 그 시간 지날때마다 멈춰줌
+          resizeMode={'cover'} // 프레임이 비디오 크기와 일치하지 않을 때 비디오 크기를 조정하는 방법을 결정합니다. cover : 비디오의 크기를 유지하면서 최대한 맞게
+          onLoad={(e: any) => {
+            console.log(e);
+            setTimeout(() => {
+              setVideoStart(true);
+            }, 5000);
+          }} // 미디어가 로드되고 재생할 준비가 되면 호출되는 콜백 함수입니다.
+          repeat={videoStart} // video가 끝나면 다시 재생할 지 여부
+          onAnimatedValueUpdate={() => {}}
+          muted={true}
+          controls={true} //바텀바가 나옴
+        />
+        {/* 바텀은 멈추는 시간대를 모두 받아오고, 비율로 계산에서 그려낼거임 */}
+        <Pressable
+          onPress={() => {
+            setVideoStart(!videoStart);
+          }}>
+          <Text>버튼</Text>
+        </Pressable>
+
+        {/* </Pressable> */}
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  fullScreen: {
+    width: '100%',
+    height: 300,
+    // position: 'absolute',
+    // top: 0,
+    // left: 0,
+    // bottom: 0,
+    // right: 0,
+  },
+});
